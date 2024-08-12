d6ec5464cdbfd7409a0e084f13bd4d5970144d6e
Amiolemen David <david@piqy.co.uk>
2024-08-12 10:14:03 +0100

    update

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -12,9 +12,8 @@ function page() {
         <p>X:-190 Y:163.3 Z:191.1</p>
       </div>
       <div className="flex items-center w-full">
-        <h2 className="font-semibold">Real AUV</h2>
+        <h2 className="font-semibold">Localized AUV</h2>
         <p>X:189.1 Y:167.5 Z:191.5</p>
-        <h2>Localized AUV</h2>
       </div>
       <div className="flex items-center justify-center pt-12">
         <Image alt="non" src={image} width={1200} height={700} />
