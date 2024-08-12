a5853d15dc9e9f3793ac4d9f69fb7d15abeb99aa
Amiolemen David <david@piqy.co.uk>
2024-08-12 10:13:31 +0100

    updatre

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -5,9 +5,21 @@ import Image from "next/image";
 function page() {
   const image = require("../../../public/images/Group1.PNG");
   return (
-    <div className="flex items-center justify-center pt-12">
-      <Image alt="non" src={image} width={1200} height={700} />
-    </div>
+    <>
+      <h1 className="text-6xl my-4  ">UWSN DATA TRANSFER RESULTS</h1>
+      <div className="flex items-center  ">
+        <h2 className="font-semibold">Real AUV</h2>
+        <p>X:-190 Y:163.3 Z:191.1</p>
+      </div>
+      <div className="flex items-center w-full">
+        <h2 className="font-semibold">Real AUV</h2>
+        <p>X:189.1 Y:167.5 Z:191.5</p>
+        <h2>Localized AUV</h2>
+      </div>
+      <div className="flex items-center justify-center pt-12">
+        <Image alt="non" src={image} width={1200} height={700} />
+      </div>
+    </>
   );
 }
 
